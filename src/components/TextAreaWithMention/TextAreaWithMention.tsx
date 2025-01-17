d4cab75ebe7dc5c2be2695194cf592a7dd29ec52
Mateusz Wąsik <wasik.matt@gmail.com>
2025-01-17 11:20:07 +0100

    feat: add improved caret position watcher

---
+++ src/components/TextAreaWithMention/TextAreaWithMention.tsx
@@ -0,0 +1,172 @@
+import { useDebounce } from '@/hooks/useDebounce';
+import { Note } from '@/hooks/useGet';
+import { useSaveNote } from '@/hooks/useSaveNote';
+import React, { useState, useRef, useEffect } from 'react';
+
+type MentionTextareaProps = {
+	note: Note;
+};
+
+export const MentionTextarea = ({ note }: MentionTextareaProps) => {
+	const [inputValue, setInputValue] = useState(note.body ?? '');
+	const saveNote = useSaveNote(note.id);
+	const debouncedValue = useDebounce(inputValue, 500);
+	const previousValue = useRef<string>(note.body ?? '');
+	const [carretsOffsetPosition, setCarretsOffsetPosition] = useState(0);
+
+	const [showSuggestions, setShowSuggestions] = useState(false);
+	const [caretCoordinates, setCaretCoordinates] = useState({ top: 0, left: 0 });
+	const [users] = useState([
+		'John Doe',
+		'Jane Smith',
+		'Alice Johnson',
+		'Mateusz Ja',
+		'Radomir Ty',
+		'Agata Ona',
+		'Mis Uszat',
+	]);
+	const contentEditableDivRef = useRef<HTMLDivElement>(null);
+
+	const handleOnChange = (event: React.ChangeEvent<HTMLDivElement>) => {
+		const textValue = event.target.textContent;
+		if (textValue === null) return;
+		setInputValue(textValue);
+		localStorage.setItem('note-edit', textValue);
+	};
+
+	useEffect(() => {
+		if (!localStorage.getItem('note-id')) {
+			localStorage.setItem('note-id', note.id);
+		}
+
+		localStorage.setItem('note-edit', debouncedValue);
+	}, [note.id, debouncedValue]);
+
+	useEffect(() => {
+		if (debouncedValue && debouncedValue !== previousValue.current) {
+			saveNote(debouncedValue);
+			previousValue.current = debouncedValue;
+		}
+	}, [debouncedValue, saveNote, inputValue]);
+
+	const handleOnInput = (event: React.FormEvent<HTMLDivElement>) => {
+		const userText = (event.target as HTMLDivElement).textContent;
+
+		if (userText === null || contentEditableDivRef.current === null) return;
+
+		const caretPosition = window.getSelection()?.anchorOffset || 0;
+		const divPosition = contentEditableDivRef.current?.getBoundingClientRect();
+
+		if (divPosition === undefined) return;
+
+		if (caretPosition) {
+			if (
+				userText[
+					getCaretCharacterOffsetWithin(contentEditableDivRef.current) - 1
+				] === '@'
+			) {
+				const caretCoord = {
+					top:
+						window.getSelection()?.getRangeAt(0).getBoundingClientRect().top ||
+						0,
+					left:
+						window.getSelection()?.getRangeAt(0).getBoundingClientRect().left ||
+						0,
+				};
+				const textareaDivCoordinates = {
+					top: divPosition.top,
+					left: divPosition.left,
+				};
+				setCaretCoordinates({
+					top: caretCoord.top - textareaDivCoordinates.top,
+					left: caretCoord.left - textareaDivCoordinates.left,
+				});
+
+				setShowSuggestions(true);
+			}
+		}
+		handleOnChange(event as React.ChangeEvent<HTMLDivElement>);
+	};
+
+	const handleOnKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+		if (event.key === 'Backspace') {
+			const caretPosition = window.getSelection()?.anchorOffset || 0;
+			const userText = contentEditableDivRef.current?.textContent;
+
+			if (userText?.[caretPosition - 1] === '@') {
+				setShowSuggestions(false);
+			}
+		}
+	};
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const getCaretCharacterOffsetWithin = (element: HTMLElement) => {
+		let caretOffset = 0;
+		if (typeof window.getSelection != 'undefined') {
+			const windowSelection = window.getSelection();
+			const windowSelectionRange = window.getSelection()?.getRangeAt(0);
+
+			if (windowSelection !== null && windowSelection.rangeCount > 0) {
+				if (windowSelectionRange === undefined) return 0;
+				const preCaretRange = windowSelectionRange.cloneRange();
+				preCaretRange.selectNodeContents(element);
+				preCaretRange.setEnd(
+					windowSelectionRange.endContainer,
+					windowSelectionRange.endOffset
+				);
+				caretOffset = preCaretRange.toString().length;
+			}
+		}
+		return caretOffset;
+	};
+
+	const showCaretPos = () => {
+		if (contentEditableDivRef.current === null) return;
+		setCarretsOffsetPosition(
+			getCaretCharacterOffsetWithin(
+				contentEditableDivRef.current as HTMLElement
+			)
+		);
+	};
+
+	return (
+		<div className='relative h-full w-full'>
+			<div
+				className=' w-full max-w-[700px] h-full overflow-y-scroll whitespace-pre-wrap break-words  border focus-visible:outline-none border-gray-300 rounded-md p-3'
+				contentEditable
+				ref={contentEditableDivRef}
+				onKeyDown={handleOnKeyDown}
+				onInput={handleOnInput}
+				onKeyUp={showCaretPos}
+				onMouseUp={showCaretPos}
+			></div>
+
+			<div className='absolute bottom-0 left-1'>
+				Caret position: {carretsOffsetPosition}
+			</div>
+
+			{showSuggestions && (
+				<div
+					className='absolute bg-white shadow-md w-[200px]'
+					style={{
+						top: caretCoordinates.top + 20,
+						left: caretCoordinates.left + 10,
+					}}
+				>
+					{users.map((user, index) => {
+						if (index >= 5) return;
+						return (
+							<div
+								key={user}
+								className='p-2 hover:bg-gray-200'
+								onClick={() => setShowSuggestions(false)}
+							>
+								{user}
+							</div>
+						);
+					})}
+				</div>
+			)}
+		</div>
+	);
+};
